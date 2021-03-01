597070b3d2fd3e43a87c2621330f787619d3c875
Timothy J. Aveni <me@timothyaveni.com>
2021-03-01 00:17:07 -0800

    RN async storage package update

--- js/persistence.ts
+++ js/persistence.ts
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react';
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import DownloadManager from './download-manager';
 import CourseData from './course-data';
 
