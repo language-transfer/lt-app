7d6fa18585d189a7b43ae3ee70c4d8aca9c5698c
jafayer <github@fayer.me>
2022-05-25 19:15:53 -0400

    Added status bar height package and fixed back button on iOS

--- js/components/Listen/ListenHeader.react.tsx
+++ js/components/Listen/ListenHeader.react.tsx
@@ -7,6 +7,7 @@ import {useNavigation} from '@react-navigation/native';
 import {TouchableNativeFeedback} from 'react-native-gesture-handler';
 import CourseData from '../../course-data';
 import { MainNavigationProp } from '../App.react';
+import {getStatusBarHeight} from 'react-native-status-bar-height';
 
 const ListenHeader = ({course}: {course: Course}) => {
   const {pop} = useNavigation<MainNavigationProp<'Listen'>>();
@@ -17,18 +18,22 @@ const ListenHeader = ({course}: {course: Course}) => {
         styles.header,
         {backgroundColor: CourseData.getCourseUIColors(course).background},
       ]}>
-      <TouchableNativeFeedback
-        style={styles.backButton}
-        onPress={() => pop()}
-        // @ts-ignore
-        background={TouchableNativeFeedback.Ripple(null, true)}>
-        <Icon
-          name="arrow-left"
-          type="font-awesome-5"
-          size={18}
-          color={CourseData.getCourseUIColors(course).text}
-        />
-      </TouchableNativeFeedback>
+      <View style={[
+        styles.backButtonContainer,
+        ]}>
+        <TouchableNativeFeedback
+          style={styles.backButton}
+          onPress={() => pop()}
+          // @ts-ignore
+          background={TouchableNativeFeedback.Ripple(null, true)}>
+          <Icon
+            name="arrow-left"
+            type="font-awesome-5"
+            size={30}
+            color={CourseData.getCourseUIColors(course).text}
+          />
+        </TouchableNativeFeedback>
+      </View>
     </View>
   );
 };
@@ -36,7 +41,7 @@ const ListenHeader = ({course}: {course: Course}) => {
 const styles = StyleSheet.create({
   header: {
     alignItems: 'flex-start',
-    marginTop: StatusBar.currentHeight,
+    paddingTop: getStatusBarHeight(),
   },
   backButton: {
     width: 56,
@@ -44,6 +49,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     borderRadius: 28,
   },
+  backButtonContainer: {
+    width: 56,
+    height: 56,
+    justifyContent: 'center',
+    borderRadius: 28,
+  }
 });
 
 export default ListenHeader;
