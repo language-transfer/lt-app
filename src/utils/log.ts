4170fe2404a12b2290e29cec3c7b0560aaf0746e
Timothy J. Aveni <me@timothyaveni.com>
2025-12-23 10:34:52 -0500

    Add useful logging metadata

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -6,10 +6,13 @@ import {
   genPreferenceAllowDataCollection,
 } from "@/src/storage/persistence";
 import { useCallback, useMemo } from "react";
+import { Platform } from "react-native";
+import DeviceInfo from "react-native-device-info";
 import {
   useCurrentCourseIfPresent,
   useCurrentLessonIfPresent,
 } from "../hooks/useCourseLessonData";
+const appVersion = DeviceInfo.getVersion();
 
 const LOG_ENDPOINT = "https://metrics.languagetransfer.org/log";
 
@@ -27,7 +30,18 @@ export const log = async (data: Record<string, any>): Promise<void> => {
     data.metadata_version = CourseData.getMetadataVersion(data.course);
   }
 
-  console.log('LOG', data);
+  const withContext = {
+    local_time: Date.now(),
+    timezone_offset: new Date().getTimezoneOffset(),
+    user_token,
+    device_os: Device.osName,
+    device_os_version: Device.osVersion,
+    platform: Platform.OS,
+    app_version: appVersion,
+    ...data,
+  };
+
+  console.log("LOG", withContext);
 
   try {
     await fetch(LOG_ENDPOINT, {
